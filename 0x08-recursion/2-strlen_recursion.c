ef4769fea49787ad3df28a23d6297f29236b0c43
Bondedincome <67425255+Bondedincome@users.noreply.github.com>
2023-09-29 22:36:38 +0300

    Hot Fix

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen_recursion - length of string
+ * @s: The source code
+ *
+ * Return: length of string
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	s++;
+	return (_strlen_recursion(s) + 1);
+}
---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * factorial - calculate factorial value
+ * @n: number to be factorialed
+ *
+ * Return: an integer result of the factorial
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 1)
+	{
+		return (1);
+	}
+	return (n * factorial(n - 1));
+}
